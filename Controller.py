3e46131f7f98b3ba829c85f2dcd43da3d9322998
Kristina <kristina.suvari@kehtna.edu.ee>
2022-03-22 09:56:40 +0200

    Additional label info and initial database file added etc.

--- Controller.py
+++ Controller.py
@@ -1,10 +1,4 @@
-#user first sends the requests to controller, handles the request flow; should not contain much code
-# 1. controller asks from Model information based on the request
-# Controller never handles data logic
-# Controller gets data from Model and sends to view
-
 from tkinter import messagebox
-import tkinter
 from Model import *
 from View import *
 
@@ -14,7 +8,8 @@ class Controller:
         self.model = Model()
         self.view = View(self)
         self.show_words()
-        
+
+
     def main(self):
         self.view.main() 
         
@@ -23,35 +18,28 @@ class Controller:
         content = self.view.textbox.selection_get()
         s0 = self.view.textbox.index("sel.first")
         s1 = self.view.textbox.index("sel.last")
-        ''' print(s0) #starting row
-        print(s1) #ending row 
-        lines = self.view.textbox.get("1.0", tkinter.END).splitlines()
-        print(lines)
-        ''' 
         print(s0)
         print(s1)
         index = s0.split('.')[0]
-        #index = s0[0]
         self.model.delete(content)
         self.update()
         messagebox.showinfo('Teade','Kirje on kustutatud!') 
-        #self.model.insert()
-        
+
+
     def show_words(self):
         self.model.get_all_words()
         arr = self.model.words
         result = ""
         for i in arr:
-            #print(i, end = ' ')
             result+=(i[0]+"\n")
-
         self.view.textbox.insert(INSERT, result)
         self.view.textbox.configure(state='disabled')
-        
+
+
     def btn_add_click(self):
        self.model.insert(self.view.userinput)  
        self.update()
-       pass
+
    
     def update(self):
        self.view.textbox.configure(state='normal')
--- View.py
+++ View.py
@@ -1,10 +1,5 @@
-# View is a templete file that views compnents based on the data that the Controller sends 
-# View sends the final presentation to Controller 
-#any interaction between Model and View go through Controller
-
 from tkinter import *
 import tkinter.font as tkFont
-#from Controller import Controller
 
 class View(Tk): 
     
@@ -17,40 +12,44 @@ class View(Tk):
         self.resizable(0,0) 
         self.title('Andmebaasi uuendamine')
         
-        
-        
         self.top_frame = self.create_top_frame()
         self.bottom_frame = self.create_bottom_frame()
         
         self.delete_button()
         self.char_input, self.button = self.create_userinput()
         self.textbox = self.create_database_textbox()
-        
+
+
     def main(self):
         self.mainloop()
-        
+
+
     def create_top_frame(self):
+        'nupud ja sisestus'
         frame = Frame(self, height=50)
         frame.pack(expand = False, fill = X)
         return frame
-    
+
+
     def create_bottom_frame(self):
         'andmebaasi info kuvamiseks'
         frame = Frame(self) 
         frame.pack(expand = True, fill ='both')
         return frame
-    
 
-        
+
     def create_userinput(self):
-        label_info = Label(self.top_frame, text='Sisesta uus sõna:', font=self.defaultStyle)
+        label_info = Label(self.top_frame, text='Uus amet andmebaasi:', font=self.defaultStyle)
         label_info.grid(row=1, column=0, padx=5, pady=5)
+        
+        label_info2 = Label(self.top_frame, text='Tee sõna aktiivseks ja:', font=self.defaultStyle)
+        label_info2.grid(row=2, column=1, padx=5, pady=5)
         
         char_input = Entry(self.top_frame, textvariable=self.userinput, justify='center', font=self.defaultStyle)
         char_input.grid(row=1,column=1, padx=5, pady=5)
         char_input.focus()
         
-        button = Button(self.top_frame, text='Lisa tabelisse!', font=self.defaultStyle, borderwidth = 2, bg='gray', fg='white', command=lambda:self.controller.btn_add_click())
+        button = Button(self.top_frame, text='Lisa!', font=self.defaultStyle, borderwidth = 2, bg='gray', fg='white', command=lambda:self.controller.btn_add_click())
         button.grid(row=1, column=2, padx=5, pady=5)
 
         return char_input, button
@@ -58,13 +57,12 @@ class View(Tk):
     
     def delete_button(self):
         'Kustuta nupp'
-        button = Button(self.bottom_frame, text='Kustuta valitud sõna!', font=self.defaultStyle, borderwidth = 2, bg='gray', fg='white', command=lambda:self.controller.btn_del_click())
-        button.pack(padx=5,pady=5)#grid(row=0,column=0, padx=5, pady=5, sticky=EW) 
-        
+        button = Button(self.bottom_frame, text='KUSTUTA!', font=self.defaultStyle, borderwidth = 2, bg='gray', fg='white', command=lambda:self.controller.btn_del_click())
+        button.pack(padx=5,pady=5)
+
+
     def create_database_textbox(self):
-        textbox = Text(self.bottom_frame, borderwidth = 0) #, text="SIIA KUVAME ANDMEBAASI", font=self.defaultStyle
+        'Andmebaasi kuvamine'
+        textbox = Text(self.bottom_frame, borderwidth = 0)
         textbox.pack(padx=5,pady=5)
         return textbox
-    
-    
-    
